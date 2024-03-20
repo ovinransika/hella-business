5ceb1f13e56d148a852ae524ea81664c22c5a5fc
Ovin Ransika <ovinransika9172@gmail.com>
2024-03-20 18:32:00 +0530

    Update Firebase config

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -2,7 +2,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; // Correct import for getAuth
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY || process.env.API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN || process.env.AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID || process.env.PROJECT_ID,
@@ -10,6 +10,16 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID || process.env.MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID || process.env.APP_ID,
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID || process.env.MEASUREMENT_ID,
+}; */
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBevPCYNaBaiqpxkqFHokkEneyr3Dl3oKI',
+  authDomain: 'hella-business.firebaseapp.com',
+  projectId: 'hella-business',
+  storageBucket: 'hella-business.appspot.com',
+  messagingSenderId: '274387046707',
+  appId: '1:274387046707:web:fe3df724d5b826e1bb6e46',
+  measurementId: 'G-2KDC534126',
 };
 
 // Initialize Firebase
