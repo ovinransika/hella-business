a7ee251a07f3eff7a9fdd8f387c7abca7cf50100
Ovin Ransika <ovinransika9172@gmail.com>
2024-03-20 17:15:34 +0530

    Update Firebase configuration and package dependencies

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,20 +1,20 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth"; // Correct import for getAuth
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBevPCYNaBaiqpxkqFHokkEneyr3Dl3oKI",
-  authDomain: "hella-business.firebaseapp.com",
-  projectId: "hella-business",
-  storageBucket: "hella-business.appspot.com",
-  messagingSenderId: "274387046707",
-  appId: "1:274387046707:web:fe3df724d5b826e1bb6e46",
-  measurementId: "G-2KDC534126"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY || API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN || AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID || PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET || STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID || MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID || APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID || MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
-const firestore = getFirestore(app); 
+const firestore = getFirestore(app);
 
 export { app, auth, firestore };
